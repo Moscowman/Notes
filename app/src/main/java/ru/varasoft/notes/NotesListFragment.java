abcc3502d60aa85d492a710be3dcded1a7de4d95
Vadim <vraspopin@inbox.ru>
2021-01-25 17:32:43 +0300

    Урок 6

---
+++ app/src/main/java/ru/varasoft/notes/NotesListFragment.java
@@ -0,0 +1,134 @@
+package ru.varasoft.notes;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NotesListFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NotesListFragment extends Fragment {
+
+    public static final String CURRENT_NOTE = "CurrentNote";
+    Note[] notes = new Note[4];
+    private Note currentNote;
+    private boolean isLandscape;
+
+    public NotesListFragment() {
+        // Required empty public constructor
+    }
+
+    public static NotesListFragment newInstance(String param1, String param2) {
+        NotesListFragment fragment = new NotesListFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notes_list, container, false);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putParcelable(CURRENT_NOTE, currentNote);
+        super.onSaveInstanceState(outState);
+    }
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        isLandscape = getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_LANDSCAPE;
+
+        if (savedInstanceState != null) {
+            currentNote = savedInstanceState.getParcelable(CURRENT_NOTE);
+        } else {
+            currentNote = notes[0];
+        }
+
+        if (isLandscape) {
+            showLandNote(currentNote);
+        }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initList(view);
+    }
+
+    private void initList(View view) {
+        LinearLayout layoutView = (LinearLayout) view;
+        notes[0] = new Note("Заметка 1", "Траляля", "я");
+        notes[1] = new Note("Заметка 2", "Это заметка", "не я");
+        notes[2] = new Note("Заметка 3", "И это заметка", "мы");
+        notes[3] = new Note("Заметка 4", "А это - нет", "они");
+
+
+        for (int i = 0; i < notes.length; i++) {
+            Note note = notes[i];
+            TextView tv = new TextView(getContext());
+            tv.setText(note.getTitle());
+            tv.setTextSize(30);
+            layoutView.addView(tv);
+            final int fi = i;
+            tv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    currentNote = notes[fi];
+                    showNote(currentNote);
+                }
+            });
+        }
+    }
+
+    private void showNote(Note currentNote) {
+        if (isLandscape) {
+            showLandNote(currentNote);
+        } else {
+            showPortNote(currentNote);
+        }
+    }
+
+    private void showLandNote(Note currentNote) {
+        NoteFragment detail = NoteFragment.newInstance(currentNote);
+
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.note_fragment, detail);  // замена фрагмента
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.commit();
+    }
+
+    private void showPortNote(Note currentNote) {
+        Intent intent = new Intent();
+        intent.setClass(getActivity(), NoteActivity.class);
+        intent.putExtra(NoteFragment.ARG_NOTE, currentNote);
+        startActivity(intent);
+    }
+}
