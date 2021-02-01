58cda82daf9e229fe35a29f9fad8c05104352d9a
Вадим <vraspopin@inbox.ru>
2021-02-01 23:08:59 +0300

    Попытка добавления верхнего меню

--- app/src/main/java/ru/varasoft/notes/MainActivity.java
+++ app/src/main/java/ru/varasoft/notes/MainActivity.java
@@ -1,23 +1,54 @@
 package ru.varasoft.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.content_main);
+
+        initToolbar();
 
         NotesListFragment notesListFragment = NotesListFragment.newInstance();
 
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, notesListFragment)
                 .commit();
+    }
 
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem search = menu.findItem(R.id.action_search);
+        SearchView searchText = (SearchView) search.getActionView();
+        searchText.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+
+        return true;
     }
 }
