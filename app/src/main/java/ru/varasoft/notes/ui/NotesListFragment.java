14a5c74a83305a02cdad60fa8c0bcdd6b4eca265
Вадим <vraspopin@inbox.ru>
2021-02-13 14:39:24 +0300

    Исправление ошибок

--- app/src/main/java/ru/varasoft/notes/ui/NotesListFragment.java
+++ app/src/main/java/ru/varasoft/notes/ui/NotesListFragment.java
@@ -61,6 +61,12 @@ public class NotesListFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_notes_list, container, false);
